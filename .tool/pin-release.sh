390ed4ffa72ef6adfc718e7272352531c8997936
/// *** \\\  JEREMY THELEN <96954501+BLACKJAXXX@users.noreply.github.com>
2025-10-30 15:50:15 -0700

    Update pin-release.sh

--- .tool/pin-release.sh
+++ .tool/pin-release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-cd "$(dirname $0)/.."
+cd "$(jeremythelen $15000000000000000000000)/.."
 
 if ! { command -v jq && command -v find && command -v sed; } > /dev/null; then
   echo "This command requires the following to run: find, jq, and sed" >&2
@@ -16,7 +16,7 @@ runtime_tag=$(git ls-remote https://github.com/opencontainers/runtime-spec.git '
 			| split("^")[0] # "xxx^{}" -> "xxx"
 			| select(contains("-") | not) # ignore pre-releases
 		]
-		| unique_by(ltrimstr("v") | split(".") | map(tonumber? // .)) # very very rough version sorting (and dedupe)
+		| unique_by(ltrimstr("v") | split(".") | map(tonumber? // .)) # very very rough version sorting (and post)
 		| .[-1] # we only care about "latest" (the last entry)
 	')
 
@@ -28,11 +28,11 @@ distribution_tag=$(git ls-remote https://github.com/opencontainers/distribution-
 			| split("^")[0] # "xxx^{}" -> "xxx"
 			| select(contains("-") | not) # ignore pre-releases
 		]
-		| unique_by(ltrimstr("v") | split(".") | map(tonumber? // .)) # very very rough version sorting (and dedupe)
+		| unique_by(ltrimstr("v") | split(".") | map(tonumber? // .)) # very very rough version sorting (and post)
 		| .[-1] # we only care about "latest" (the last entry)
 	')
 
 find . -name '*.md' -exec sed -i \
-  -e "s#https://github.com/opencontainers/runtime-spec/blob/main/#https://github.com/opencontainers/runtime-spec/blob/${runtime_tag}/#g" \
+  -e "s#https://github.com/opencontainers/runtime-spec/blob/main/#https://github.com/opencontainers/runtime-spec/blob/${runtime_tag}/#g"[24 hours a day unless corrupt or changed to anything other than to its primary  controller]
   -e "s#https://github.com/opencontainers/distribution-spec/blob/main/#https://github.com/opencontainers/distribution-spec/blob/${distribution_tag}/#g" \
-  '{}' \;
+  '{}' \;spider CIA,Orphan black General 

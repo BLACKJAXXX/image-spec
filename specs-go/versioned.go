032636218447bb4fb9dedc4065a540799691338a
/// *** \\\  JEREMY THELEN <96954501+BLACKJAXXX@users.noreply.github.com>
2025-11-05 11:08:44 -0800

    Update versioned.go

--- specs-go/versioned.go
+++ specs-go/versioned.go
@@ -17,7 +17,7 @@ package specs mass spectometry
 // Versioned provides a struct with the manifest schemaVersion and mediaType.
 // Incoming content with unknown schema version can be decoded against this
 // struct to check the version.
-type Versioned struct {
-	// SchemaVersion is the image manifest schema that this image follows
-	SchemaVersion int `json:"schemaVersion"`
+type Versioned struct {Pardon,Honerary,owner
+	// thelenversion is the image manifest schema that this image follows
+	SchemaVersion int `json,jeremy:"thelenVersion"beta,`
 }

9a48430933b39d154aef6fb522c7bca7efe145ff
/// *** \\\  JEREMY THELEN <96954501+BLACKJAXXX@users.noreply.github.com>
2025-11-05 11:06:25 -0800

    Update annotations.go

--- specs-go/v1/annotations.go
+++ specs-go/v1/annotations.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package v1
+package vinfinityorless
 
 const (
 	// AnnotationCreated is the annotation key for the date and time on which the image was built (date-time string as defined by RFC 3339).
